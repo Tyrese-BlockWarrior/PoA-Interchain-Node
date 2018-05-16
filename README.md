4535cc3223dcc2ed11684657ca95e0b2411d3cc0
TyreseLian <tyreselian3915@gmail.com>
2018-05-16 14:37:36 +0700

    Utility to deposit on a wallet

--- README.md
+++ README.md
@@ -40,4 +40,35 @@ Wait 10 seconds between each call
 
 ## Deploy the multisig wallet on both chains
 
-   make mainchain_wallet sidechain_wallet
+    make mainchain_wallet sidechain_wallet
+
+## Sending ether to arbitrary addresses offchain
+
+The multisig wallet has a function called `deposit` which allows you to transfer ether from your address on the chain A to a receiver on the chain B.
+
+Instead of paying to the wallet address, you call the `deposit` method using geth console or our utility program `icn-deposit.go`:
+
+    go run cmd/icn-deposit/main.go -mainchain -keyjson=mainchain/keystore/<your_key_json> -password=dummy -endpoint=mainchain/geth.ipc -wallet=`cat mainchain/miner` -receiver=`cat sidechain/sealer` -value="25000000000000000000"
+
+Usage:
+
+```
+  -endpoint string
+    	URL or path of the origin chain endpoint
+  -keyjson string
+    	Path to the JSON private key file of the user
+  -mainchain
+    	Deploy the main chain wallet
+  -password string
+    	Passphrase needed to unlock the user's JSON key
+  -receiver string
+    	Ethereum address of the receiver on the target chain
+  -sidechain
+    	Deploy the side chain wallet
+  -value string
+    	Value (wei) to transfer to the receiver
+  -wallet string
+    	Ethereum address of the multisig wallet on the origin chain
+```
+
+The interchain node will notice your call and mirror the transaction on the other chain.
---
+++ cmd/icn-deposit/main.go
@@ -0,0 +1,76 @@
+// Utility to perform a transfer to another address on the other chain using the wallet
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"strings"
+
+	"github.com/WeTrustPlatform/interchain-node/bind/mainchain"
+	"github.com/WeTrustPlatform/interchain-node/bind/sidechain"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	// Command line flags
+	mainChainPtr := flag.Bool("mainchain", false, "Target the main chain wallet")
+	sideChainPtr := flag.Bool("sidechain", false, "Target the side chain wallet")
+	keyJSONPath := flag.String("keyjson", "", "Path to the JSON private key file of the user")
+	password := flag.String("password", "", "Passphrase needed to unlock the user's JSON key")
+	endpoint := flag.String("endpoint", "", "URL or path of the origin chain endpoint")
+	walletStr := flag.String("wallet", "", "Ethereum address of the multisig wallet on the origin chain")
+	receiver := flag.String("receiver", "", "Ethereum address of the receiver on the target chain")
+	value := flag.String("value", "", "Value (wei) to transfer to the receiver")
+
+	flag.Parse()
+
+	// Connect to both chains
+	client, _ := ethclient.Dial(*endpoint)
+
+	walletAddress := common.HexToAddress(*walletStr)
+
+	// Open the account key file
+	keyJSON, err := ioutil.ReadFile(*keyJSONPath)
+	if err != nil {
+		log.Fatalf("Key json read error: %v", err)
+	}
+
+	// Create a transactor
+	auth, err := bind.NewTransactor(strings.NewReader(string(keyJSON[:])), *password)
+	if err != nil {
+		log.Fatalf("Failed to create authorized transactor: %v", err)
+	}
+
+	// Set the amount to be transfered
+	v := new(big.Int)
+	v, ok := v.SetString(*value, 10)
+	if !ok {
+		fmt.Println("SetString: error")
+		return
+	}
+	auth.Value = v
+
+	// Attach the wallet and submit the transaction
+	if *mainChainPtr {
+		wallet, err := mainchain.NewMainChain(walletAddress, client)
+		wtx, err := wallet.Deposit(auth, common.HexToAddress(*receiver))
+		if err != nil {
+			log.Printf("SubmitTransaction error: %v", err)
+		}
+		log.Printf("Transaction sent: %v", wtx)
+	}
+	if *sideChainPtr {
+		wallet, err := sidechain.NewSideChain(walletAddress, client)
+		wtx, err := wallet.Deposit(auth, common.HexToAddress(*receiver))
+		if err != nil {
+			log.Printf("SubmitTransaction error: %v", err)
+		}
+		log.Printf("Transaction sent: %v", wtx)
+	}
+}
