b50dfe08a3adaf77567a5efd343df15c511acb51
TyreseLian <tyreselian3915@gmail.com>
2018-05-21 15:06:20 +0700

    Unit test for ParseSignature

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package icn
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"math/big"
 
@@ -82,6 +83,8 @@ func ParseSignature(sig []byte) (v uint8, r, s common.Hash) {
 	r = common.BytesToHash(sig[0:32])
 	s = common.BytesToHash(sig[32:64])
 	v = uint8(sig[64:65][0] + 27)
+
+	fmt.Println(common.Bytes2Hex(sig), r.Hex(), s.Hex(), v)
 
 	return
 }
--- main_test.go
+++ main_test.go
@@ -22,29 +22,49 @@ func TestMsgHash(t *testing.T) {
 		args args
 		want common.Hash
 	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MsgHash(tt.args.contractAddress, tt.args.txHash, tt.args.toAddress, tt.args.value, tt.args.data, tt.args.version); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MsgHash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseSignature(t *testing.T) {
+	type args struct {
+		sig []byte
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wantV uint8
+		wantR common.Hash
+		wantS common.Hash
+	}{
 		{
-			name: "Computes solidity compatible hash",
+			name: "Parses signature correctly",
 			args: args{
-				common.HexToAddress("75076e4fbba61f65efb41d64e45cff340b1e518a"),
-				common.HexToHash("03c85f1da84d9c6313e0c34bcb5ace945a9b12105988895252b88ce5b769f82b"),
-				common.HexToAddress("f17f52151ebef6c7334fad080c5704d77216b732"),
-				big.NewInt(100000000),
-				[]byte{},
-				1,
+				sig: common.Hex2Bytes("a27a17b20a8dcc6fedb6196b84624ce3f3961a2423642fe13003a816c383f93205adf64e0805449d18b866991ce19e5439567cd3613ae1775e90fb4a8b0cbc6800"),
 			},
-			want: common.HexToHash("6b0673bcb3726c0f7956ef57a9542ed225bfe74f1d2a75414d198d55e8956da5"),
+			wantV: 27,
+			wantR: common.HexToHash("a27a17b20a8dcc6fedb6196b84624ce3f3961a2423642fe13003a816c383f932"),
+			wantS: common.HexToHash("05adf64e0805449d18b866991ce19e5439567cd3613ae1775e90fb4a8b0cbc68"),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := MsgHash(
-				tt.args.contractAddress,
-				tt.args.txHash,
-				tt.args.toAddress,
-				tt.args.value,
-				tt.args.data,
-				tt.args.version); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("MsgHash() = %v, want %v", got, tt.want)
+			gotV, gotR, gotS := ParseSignature(tt.args.sig)
+			if gotV != tt.wantV {
+				t.Errorf("ParseSignature() gotV = %v, want %v", gotV, tt.wantV)
+			}
+			if !reflect.DeepEqual(gotR, tt.wantR) {
+				t.Errorf("ParseSignature() gotR = %v, want %v", gotR, tt.wantR)
+			}
+			if !reflect.DeepEqual(gotS, tt.wantS) {
+				t.Errorf("ParseSignature() gotS = %v, want %v", gotS, tt.wantS)
 			}
 		})
 	}
