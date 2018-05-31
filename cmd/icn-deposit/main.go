e28663c773a20f4d081912b88249a59885629f1d
TyreseLian <tyreselian3915@gmail.com>
2018-06-01 01:18:19 +0700

    License under the GPLv3 (#9)
    * License under the GPLv3
    
    * Rename COPYING to LICENSE
    
    * Add copyright notice

--- cmd/icn-deposit/main.go
+++ cmd/icn-deposit/main.go
@@ -1,5 +1,23 @@
-// Utility to perform a transfer to another address on the other chain using the wallet
+/*
+Copyright (C) 2018 WeTrustPlatform
 
+This file is part of interchain-node.
+
+interchain-node is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+interchain-node is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with interchain-node.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Utility to perform a transfer to another address on the other chain using the wallet
 package main
 
 import (
